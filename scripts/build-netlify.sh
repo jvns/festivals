f403cdf553c3169fddc30754e4ad4ceb7f9c661e
Julia Evans <julia@jvns.ca>
2025-08-11 14:12:57 -0400

    build css

--- scripts/build-netlify.sh
+++ scripts/build-netlify.sh
@@ -2,3 +2,5 @@ set -euo pipefail
 export PYTHONPATH=.
 pip install -r requirements.txt
 source scripts/generate.sh
+npm install --save-dev lightningcss-cli
+find site -name '*.css' -exec lightningcss --minify --targets '>= 0.25%' {} -o {} \;
